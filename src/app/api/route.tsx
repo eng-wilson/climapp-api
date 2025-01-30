74fa488a7e4bcff8594d07d67bb6211a9b3e580d
Wilson Carvalho <eng.wilson.c@gmail.com>
2025-01-29 21:21:32 -0300

    refactor: api format

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 import cities from "@/data/cities.json";
 
 export async function GET() {
-  return NextResponse.json({ data: cities });
+  return NextResponse.json(cities);
 }
